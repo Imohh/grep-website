db980ab486c1f3818298631c4ee458c2f0fc0147
Imoh Precious <jaypee88830@gmail.com>
2023-06-26 05:03:20 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {WOW} from 'wowjs';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 import Homepage from './Homepage'
 
 function App() {
+  useEffect(() => {
+    const wow = new WOW({
+      offset: 100,
+      mobile: false,
+      live: true,
+    });
+
+    wow.init();
+  }, []);
+
   return (
     <>
-      <Homepage />
+      <div className="
+        max-w-[2500px] 
+        mx-auto ">
+        <Navbar />
+        <BrowserRouter>
+          <Routes>
+            <Route  path='/' element={<Homepage />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer />
+      </div>
     </>
   );
 }
