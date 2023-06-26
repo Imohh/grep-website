c9745228c69ec24f6f8a4788f9f3b576f8467c32
Imoh Precious <jaypee88830@gmail.com>
2023-06-26 04:53:33 +0100

    page setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+	return (
+		<>
+			<h2 className="bg-red-500">Footer</h2>
+		</>
+	)
+}
+
+export default Footer
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+
+
+const Navbar = () => {
+	return (
+		<>
+			<h2 className="bg-red-500">Navbar</h2>
+		</>
+	)
+}
+
+export default Navbar
