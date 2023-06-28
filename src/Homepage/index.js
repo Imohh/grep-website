dd40ace018035a87b0e3db33a4c93563ef4e119e
Imoh Precious <jaypee88830@gmail.com>
2023-06-28 03:52:01 +0100

    page alignment

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -108,29 +108,29 @@ const Homepage = () => {
 			</div>
 
 			{/*THIRD SECTION*/}
-			<div className="overflow-hidden lg:h-96 md:h-fit 
+			<div className="overflow-hidden md:h-96 lg:h-96 md:h-fit 
 			grid grid-cols-1 lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-4 
-				md:py-20 px-10 lg:py-40 lg:pb-96 xl:px-40 mx-auto
+				md:pb-40 md:pt-14 px-10 lg:py-40 lg:pb-96 xl:px-40 mx-auto
 				bg-gradient-to-r from-yellow-200 to-tickle-me-pink-600">
 				<div className="-mt-20 -ml-20 md:-mt-60 md:ml-20 lg:-mt-72 lg:-ml-96">
-					<img className=" lg:-mt-72 lg:-ml-20 xl:-mt-96 xl:-ml-52" src={ge6} alt="mockup" />
-					<img className=" lg:ml-24 lg:-mt-80 xl:-mt-96" src={ge4} alt="mockup" />
-					<img className=" lg:-mt-96 lg:ml-96 xl:-mt-96 xl:ml-96" src={ge3} alt="mockup" />
+					<img className="sm:-mt-72 sm:-ml-20 md:-mt-0 md:-ml-40 lg:-mt-72 lg:-ml-20 xl:-mt-96 xl:-ml-52" src={ge6} alt="mockup" />
+					<img className="sm:-mt-72 sm:ml-24 md:-mt-36 md:-ml-20 lg:ml-24 lg:-mt-80 xl:-mt-96" src={ge4} alt="mockup" />
+					<img className="sm:-mt-72 sm:ml-96 md:-mt-52 md:ml-24 lg:-mt-96 lg:ml-96 xl:-mt-96 xl:ml-96" src={ge3} alt="mockup" />
 				</div>
-				<div className=" mt-0 py-0 lg:py-auto pr-0 lg:pr-18 ">
+				<div className="-mt-72 pb-20 lg:pb-0 md:pb-0 xl:pb-0 md:mt-0 xl:mt-0 lg:mt-0 py-0 lg:py-auto pr-0 lg:pr-18 ">
 					<h2 className="
 						text-white text-center md:text-left lg:text-left text-3xl 
 						lg:text-5xl capitalize pb-5 md:pb-10 font-semibold px-5 lg:px-0 md:px-0">
 					Get the Grep app for your mobile devices</h2>
 					<p className="text-white md:text-left text-center text-lg md:text-xl lg:text-xl xl:text-xl mb-10">Download the Grep app today and let us run those errands for you with in no time. It's free!</p>
-					<div>
-						<button className="mb-5 lg:mb-0 xl:mb-0 w-full py-1 px-5 lg:w-52 xl:w-56 text-left bg-tickle-me-pink-600 rounded-full mr-5 text-white text-lg">
+					<div className="">
+						<button className="mb-5 lg:mb-0 xl:mb-0 w-full py-1 px-5 md:w-3/6 lg:w-52 xl:w-56 text-left bg-tickle-me-pink-600 rounded-full mr-5 text-white text-base md:text-base xl:text-lg lg:text-lg">
 							<div className="flex justify-center items-center">
-								<img  className="w-10 pt-2 ml-2 mr-5 float-left " src={googleplay} />
-								<span className=""><span className="text-xl">Download</span> for Android</span>
+								<img  className="w-10 pt-2 ml-2 mr-2 xl:mr-5 lg:mr-5 float-left " src={googleplay} />
+								<span className=""><span className="md:text-lg xl:text-xl lg:text-xl">Download</span> for Android</span>
 							</div>
 						</button>
-						<button className="w-full text-center py-1 px-5 lg:w-52 xl:w-56 text-left bg-white rounded-full text-tickle-me-pink-600 text-lg">
+						<button className="w-full text-center py-1 px-5 md:w-2/5 lg:w-52 xl:w-56 text-left bg-white rounded-full text-tickle-me-pink-600 text-base md:text-base xl:text-lg lg:text-lg">
 							<div className="flex justify-center items-center">
 								<img className=" w-10 ml-2 mr-5 pt-2 float-left" src={appstore} />
 								<span className=""><span className="text-xl">Download</span> for iOS</span>
@@ -203,7 +203,9 @@ const Homepage = () => {
 					md:text-left lg:text-left text-center font-semibold text-port-gore-600">We are ready to attend to your orders</h2>
 					<p className="wow fadeInRight text-port-gore-600 md:text-left text-center text-lg md:text-xl mb-10">Our errand persons are always happy to receive your orders. 
 					You can set reminders and we will be sure to remind you at all times. It's easy and straight forward.</p>
-					<button className="wow fadeInRight">get started</button>
+					<div className="flex justify-center lg:justify-start xl:justify-start">
+						<button className="wow fadeInRight">get started</button>
+					</div>
 				</div>
 			</div>
 		</>
